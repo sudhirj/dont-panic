fd63d11cab904d6b3a64848aeeaf4a61d0a7e138
Rohit Garg <rohitgarg19@gmail.com>
2013-01-08 16:15:48 +0530

    Raised error if username password are incorrect

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,13 +1,17 @@
 class DataController < ApplicationController
   def index
     admin_user = params[:username].present? ? AdminUser.find_by_email(params[:username]) : current_admin_user
-    admin_user = nil if params[:username].present? && admin_user.present? && !admin_user.valid_password?(params[:password])
-    @countries = Country.all
-    @cities = City.all
-    @companies = Company.where(:admin_user_id => admin_user.id) if admin_user.present?
-    @offices = Office.where(:company_id => @companies.collect(&:id)) if @companies.present?
-    @places = Place.all
-    @administrators = Administrator.where(:office_id => @offices.collect(&:id)) if @offices.present?
-    @place_types = PlaceType.all
+    if params[:username].present? && (admin_user.present? && 
+                !admin_user.valid_password?(params[:password]) || admin_user.nil?)
+        render :text => "Invalid username/password supplied.", :status => :unauthorized 
+    else 
+        @countries = Country.all
+        @cities = City.all
+        @companies = Company.where(:admin_user_id => admin_user.id) if admin_user.present?
+        @offices = Office.where(:company_id => @companies.collect(&:id)) if @companies.present?
+        @places = Place.all
+        @administrators = Administrator.where(:office_id => @offices.collect(&:id)) if @offices.present?
+        @place_types = PlaceType.all
+    end
   end
 end
